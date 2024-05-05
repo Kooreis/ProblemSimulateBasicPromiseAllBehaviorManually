82e404d3dc4e766b6b75bd4b4d9467f944e3c021
Kooreis <kooreis.official@gmail.com>
2024-05-05 16:55:05 -0500

    Initial setup of the promiseAll function. This function takes an array of promises as an argument and returns a new promise. Two variables, results and count, are also initialized.

--- solution.cs
+++ solution.cs
@@ -1,34 +1,6 @@
-```javascript
 function promiseAll(promises) {
     return new Promise((resolve, reject) => {
         let results = [];
         let count = 0;
-
-        for (let i = 0; i < promises.length; i++) {
-            promises[i]
-                .then((value) => {
-                    results[i] = value;
-                    count++;
-
-                    if (count === promises.length) {
-                        resolve(results);
-                    }
-                })
-                .catch((error) => {
-                    reject(error);
-                });
-        }
     });
 }
-
-// Test the function
-let promise1 = Promise.resolve(3);
-let promise2 = new Promise((resolve, reject) => setTimeout(resolve, 100, 'foo'));
-let promise3 = 42;
-
-promiseAll([promise1, promise2, promise3]).then(values => {
-    console.log(values); // expected output: Array [3, "foo", 42]
-}).catch(error => {
-    console.log(error);
-});
-```
